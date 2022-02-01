623f6052ae23caf9bdaa10e87dea0914922d2a92
MelAnonuevo <97712598+MelAnonuevo@users.noreply.github.com>
2022-02-01 03:16:27 -0500

    Commit

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let userController = require('../controllers/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.user);
 
 /* GET users listing. */
-router.get('/mel', function(req, res, next) {
-  res.send('Welcome Mel');
-});
+router.get('/mel', userController.mel);
 
 module.exports = router;
