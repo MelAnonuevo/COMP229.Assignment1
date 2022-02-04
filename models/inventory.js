44b1a88a7fda782fe6ab10608af5ddf921eed6b2
MelAnonuevo <97712598+MelAnonuevo@users.noreply.github.com>
2022-02-04 01:41:14 -0500

    Assignment1-Commit

--- models/inventory.js
+++ models/inventory.js
@@ -11,7 +11,7 @@ let inventoryModel = mongoose.Schema(
             w: Number,
             uom: String
         },
-        status: String,
+        status: String
     },
     {
         collection: "inventory"
