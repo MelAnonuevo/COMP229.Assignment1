32879f9b282f32a2b2e86482ab982f15cbaa7ac8
MelAnonuevo <97712598+MelAnonuevo@users.noreply.github.com>
2022-02-10 20:23:38 -0500

    2/10/22

---
+++ controllers/inventory.js
@@ -0,0 +1,24 @@
+let Inventory = require('../models/inventory');
+
+exports.list = function(req, res, next){
+    Inventory.find((err, inventoryList) => {
+        // console.log(inventoryList);
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            // console.log(inventoryList);
+            res.render(
+                'inventory/list',
+             {
+                 title: 'Inventory List',
+                InventoryList: inventoryList
+            }
+            );
+        }
+    });
+    // res.render('index', {title: 'About'});
+}
+
